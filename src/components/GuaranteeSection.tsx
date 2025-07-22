11b6293942766988e51946011045bede66310e76
gabrielvinha <64176098+gabrielvinha@users.noreply.github.com>
2025-07-22 16:01:56 +0200

    Updated GuaranteeSection.tsx

--- src/components/GuaranteeSection.tsx
+++ src/components/GuaranteeSection.tsx
@@ -4,7 +4,7 @@ export const GuaranteeSection: React.FC = () => {
   return (
     <section className="flex flex-col items-center px-50 sm:px-60 md:px-80">
        
-      <h2 className="text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.4] text-center mt-8 sm:mt-10 md:mt-12">
+      <h2 className="text-black text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.4] text-center mt-8 sm:mt-10 md:mt-12">
         Garantia incondicional de 15 dias
       </h2>
       <p className="text-[rgba(223,223,223,1)] text-base sm:text-lg md:text-xl font-normal text-center mt-6 sm:mt-8 md:mt-10">
